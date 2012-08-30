83515ef15a5f2b84989fb07af282e843abf23117
Robert Krahn <robert.krahn@gmail.com>
2012-08-29 18:37:59 -0700

    setting user $USER after install

--- package-scripts/run-app.sh
+++ package-scripts/run-app.sh
@@ -3,5 +3,7 @@
 # important to not be in install directory, jsDAV throws some error when in
 # "wrong" environment
 export TMPDIR=
+app=/Applications/livelykernel-scripts.app
 cd $HOME
-open /Applications/livelykernel-scripts.app
+sudo chown -R $USER $app
+open $app
